b7ea073d1eeb6d97cb1f4497e221a6e0ae029da1
edkrueger <edkrueger@gmail.com>
2020-11-19 13:19:41 -0600

    add app

---
+++ app.js
@@ -0,0 +1,8 @@
+d3.json("owid-covid-2020-11-19-usa.json").then(
+    (data) => console.log(data)
+)
+d3.csv("owid-covid-2020-11-19-usa.csv").then(
+    (data) => console.log(data)
+)
+
+console.log("Hello")
